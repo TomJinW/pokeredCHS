d3c98959f03c3384c3393785b2d319d99dd23795
Tom Choi <jw95@live.cn>
2023-07-22 19:43:21 -0700

    kill enemies by pressing UP in debug rom

--- home/npc_movement.asm
+++ home/npc_movement.asm
@@ -62,3 +62,33 @@ IF DEF(_DEBUG)
 	bit BIT_B_BUTTON, a
 ENDC
 	ret
+
+; DebugPressedOrHeldSELECT::
+; IF DEF(_DEBUG)
+; 	ldh a, [hJoyHeld]
+; 	bit BIT_SELECT, a
+; 	ret nz
+; 	ldh a, [hJoyPressed]
+; 	bit BIT_SELECT, a
+; 	ret
+; ENDC
+
+; DebugPressedOrHeldA::
+; IF DEF(_DEBUG)
+; 	ldh a, [hJoyHeld]
+; 	bit BIT_A_BUTTON, a
+; 	ret nz
+; 	ldh a, [hJoyPressed]
+; 	bit BIT_A_BUTTON, a
+; 	ret
+; ENDC
+
+DebugPressedOrHeldUP::
+IF DEF(_DEBUG)
+	ldh a, [hJoyHeld]
+	bit BIT_D_UP, a
+	ret nz
+	ldh a, [hJoyPressed]
+	bit BIT_D_UP, a
+	ret
+ENDC
