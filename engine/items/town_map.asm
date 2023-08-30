b98bf8af93e1d76c06c5916734e3e1987abc6843
Tom Choi <jw95@live.cn>
2023-08-30 03:18:06 -0700

    Update town_map.asm

--- engine/items/town_map.asm
+++ engine/items/town_map.asm
@@ -133,7 +133,11 @@ LoadTownMap_Nest:
 	jr z, .shortnest
 	ld de, MonsNestText
 .shortnest
+	ld a, [wENGNameMark]
+	cp 1
+	jr z, .skipAdding
 	call PlaceString
+.skipAdding
 	call DecreaseDFSStack
 	call WaitForTextScrollButtonPress
 	call ExitTownMap
